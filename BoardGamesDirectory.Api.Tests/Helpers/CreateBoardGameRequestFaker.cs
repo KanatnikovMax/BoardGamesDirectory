80f39e4a28274635b7ca614c5fe613eff1b5aae2
kanatnikovmaxim@yandex.ru <kanatnikovmaxim@yandex.ru>
2025-07-17 01:55:06 +0300

    add validators tests

---
+++ BoardGamesDirectory.Api.Tests/Helpers/CreateBoardGameRequestFaker.cs
@@ -0,0 +1,26 @@
+﻿using AutoBogus;
+using BoardGamesDirectory.Api.Controllers.BoardGames.Entities;
+
+namespace BoardGamesDirectory.Api.Tests.Helpers;
+
+internal static class CreateBoardGameRequestFaker
+{
+    private static readonly string ValidName = "DnD";
+    private static readonly string ValidGenre = "НРИ";
+    private static readonly string ValidPublisher = "Hobby World";
+    private static readonly string ValidDescription = "Самая популярная НРИ";
+    private static readonly int ValidMinAge = 6;
+    private static readonly int ValidPrice = 3999;
+
+    public static CreateBoardGameRequest GenerateValid()
+    {
+        return new AutoFaker<CreateBoardGameRequest>()
+            .RuleFor(x => x.Name, f => ValidName)
+            .RuleFor(x => x.Genre, f => ValidGenre)
+            .RuleFor(x => x.Publisher, f => ValidPublisher)
+            .RuleFor(x => x.Description, f => ValidDescription)
+            .RuleFor(x => x.MinAge, f => ValidMinAge)
+            .RuleFor(x => x.Price, f => ValidPrice)
+            .Generate();
+    }
+}
